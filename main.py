47d799a040a17b693220a23267a2d3c69e0e186d
ruslanaytmuratovv <russgg53@gmail.com>
2026-03-17 17:36:14 +0500

    RUSLAN

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv('BOT_TOKEN')
+bot = Bot(token=token)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(msg:Message):
+    await msg.answer('salem ruslan')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
